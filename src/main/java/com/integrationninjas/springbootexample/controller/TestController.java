0c030a905e2ab83078bc89898f4d0908bcfcce1c
Shardul Negi <shardul.negi21@gmail.com>
2024-06-30 17:25:49 -0700

    push change
    push change

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -12,8 +12,8 @@ public class TestController {
 	@GetMapping
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
-		object.put("name", "Integration Ninjas");
-		object.put("email", "integrationninjas@gmail.com");
+		object.put("name", "Shardul Negi");
+		object.put("email", "shardul@gmail.com");
 		return object;
 	}
 
