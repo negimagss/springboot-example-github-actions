ab88793209b39f6a54686865268fdafd839cfee1
Shardul Negi <shardul.negi21@gmail.com>
2024-06-30 20:56:36 -0700

    push more change
    push more change

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -1,4 +1,6 @@
 package com.integrationninjas.springbootexample.controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +15,24 @@ public class TestController {
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
 		object.put("name", "Shardul Negi");
-		object.put("email", "shardul@gmail.com");
+		object.put("text", "First Time using Github Actions");
 		return object;
 	}
+
+	@PostMapping("/postvalue")
+    public Object postValue(@RequestBody Map<String, Integer> requestBody) {
+        // Process the received input
+        int a = requestBody.get("a");
+        int b = requestBody.get("b");
+
+        // Modify the response
+        Map<String, Object> responseObject = new HashMap<>();
+        responseObject.put("sum", a+b);
+        responseObject.put("multiply", a*b);
+		responseObject.put("divide", a/b);
+		responseObject.put("subtract", a-b);
+
+        return responseObject;
+    }
 
 }
